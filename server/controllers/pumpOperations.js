1d7a070e473919421d924417cbd556132102fa0d
Abhishek <hello@abhishekpatnaik.com>
2018-10-23 18:05:09 +0530

    Modified pump

--- server/controllers/pumpOperations.js
+++ server/controllers/pumpOperations.js
@@ -11,38 +11,38 @@ router.use(function (req, res, next) {
 });
 
 router.post('/onOperation', (req, res) => {
-      var startTime=moment().valueOf();
-      var newOperation = new PumpOperation({
-        deviceId: req.body.deviceId,
-        onTime:startTime,
-        offTime:startTime.add(2,'h'),
-        location: req.body.location
-      });
-      newOperation.save().then(pumpOperation => {
-        res.send({pumpOperation});
-      })
-
-  }).catch((err) => {
-    res.status(400).send();
-  })
-});
+  var startTime = moment().valueOf();
+  var newOperation = new PumpOperation({
+    deviceId: req.body.deviceId,
+    onTime: startTime,
+    offTime: startTime.add(2, 'h'),
+    location: req.body.location
+  });
+  newOperation.save()
+    .then(pumpOperation => {
+      res.send({pumpOperation});
+    })
+    .catch((err) => {
+      res.status(400).send();
+    })
+})
 
 
 router.put('/offOpeartion/:operationId', (req, res) => {
-  var body={
+  var body = {
     offTime: moment().valueOf()
   }
-  PumpOperation.findOneAndUpdate({operationId:req.body.operationId},{$set:body},{new:true}).then((pumpOperation)=>{
+  PumpOperation.findOneAndUpdate({operationId: req.body.operationId}, {$set: body}, {new: true}).then((pumpOperation) => {
     res.send({pumpOperation});
   }).catch((err) => {
     res.status(400).send();
   })
 });
 
-router.get('allOperations/:deviceId',(req,res)=>{
-  PumpOperation.find({'deviceId':req.params.deviceId}).then((operations)=>{
+router.get('allOperations/:deviceId', (req, res) => {
+  PumpOperation.find({'deviceId': req.params.deviceId}).then((operations) => {
     res.send({operations});
-  }).catch(e=>{
+  }).catch(e => {
     res.status(400).send();
   })
 });
