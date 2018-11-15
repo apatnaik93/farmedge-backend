4b3a10dd2d811de35b43eecabe1d796c16ae0fd2
Abhi <abhi@Abheepsas-Air.Dlink>
2018-11-15 15:59:53 +0530

    Done

--- server/models/User.js
+++ server/models/User.js
@@ -2,24 +2,24 @@ const mongoose = require('mongoose');
 const randomString = require('random-string');
 
 var userSchema = new mongoose.Schema({
-  userId: {
-    type: String,
-    unique: true
-  },
-  name: {
-    type: String
-  },
-  email: {
-    type: String,
-
-  },
-  mobile: {
-    type: String,
-    unique: true
-  },
-  location: {
-    type: String
-  }
+    userId: {
+        type: String,
+        unique: true
+    },
+    name: {
+        type: String
+    },
+    email: {
+        type: String,
+        unique: true
+    },
+    mobile: {
+        type: String,
+        unique: true
+    },
+    location: {
+        type: String
+    }
 });
 
 var User = module.exports = mongoose.model('User', userSchema);
