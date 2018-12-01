179100c5ea662e726d41bd57f875877578970838
Abhi <abhi@Abheepsas-Air.Dlink>
2018-12-01 18:19:39 +0530

    Message

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -4,46 +4,59 @@ const User = require('./../models/User');
 const router = require('express').Router();
 
 router.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
-  next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
+    next();
 });
 
 function makeid() {
-  var text = "";
-  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  for (var i = 0; i < 8; i++)
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
-  return text;
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for (var i = 0; i < 8; i++)
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    return text;
 }
 
 router.post('/registerUser', (req, res) => {
-  User.findOne({'mobile': req.body.mobile}).then(user => {
-    if (user) {
-      res.send({user});
-    } else {
-      var newUser = new User({
-        userId: "USR" + makeid() + Date.now(),
+    User.findOne({'mobile': req.body.mobile}).then(user => {
+        if (user) {
+            res.send({user});
+        } else {
+            var newUser = new User({
+                userId: "USR" + makeid() + Date.now(),
+                name: req.body.name,
+                mobile: req.body.mobile,
+                email: req.body.email,
+                location: req.body.location
+            });
+            newUser.save().then(user => {
+                res.send({user});
+            });
+        }
+    }).catch((err) => {
+        res.status(400).send();
+    })
+});
+
+router.put('/updateUser', (req, res) => {
+    var user = {
         name: req.body.name,
-        mobile: req.body.mobile,
         email: req.body.email,
         location: req.body.location
-      });
-      newUser.save().then(user => {
+    };
+    User.findOneAndUpdate({'mobile': req.body.mobile}, {$set: user}, {new: true}).then((user) => {
         res.send({user});
-      });
-    }
-  }).catch((err) => {
-    res.status(400).send();
-  })
+    }).catch((err) => {
+        res.status(400).send();
+    })
 });
 
 router.get('/getUser/:mobile', (req, res) => {
-  User.findOne({'mobile': req.params.mobile}).then(user => {
-    res.send({user});
-  }).catch((err) => {
-    res.status(400).send();
-  })
+    User.findOne({'mobile': req.params.mobile}).then(user => {
+        res.send({user});
+    }).catch((err) => {
+        res.status(400).send();
+    })
 });
 
 module.exports = router;
