1e95cfe7fa68d4970c9ff6f15d86edf80b5cd833
Abhi <abhi@Abheepsas-Air.Dlink>
2018-12-04 10:06:20 +0530

    Working

--- server/controllers/devices.js
+++ server/controllers/devices.js
@@ -3,46 +3,59 @@ const Device = require('./../models/Device');
 const router = require('express').Router();
 
 router.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
-  next();
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
+    next();
 });
 
 function makeid() {
-  var text = "";
-  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  for (var i = 0; i < 8; i++)
-    text += possible.charAt(Math.floor(Math.random() * possible.length));
-  return text;
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    for (var i = 0; i < 8; i++)
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+    return text;
 }
 
 router.post('/registerDevice', (req, res) => {
-  Device.findOne({'simNumber': req.body.simNumber}).then(device => {
-    if (device) {
-      res.send({device});
-    } else {
-      var newDevice = new Device({
-        deviceId: "DVS" + makeid() + Date.now(),
-        userId: req.body.userId,
+    Device.findOne({'simNumber': req.body.simNumber}).then(device => {
+        if (device) {
+            res.send({device});
+        } else {
+            var newDevice = new Device({
+                deviceId: "DVS" + makeid() + Date.now(),
+                userId: req.body.userId,
+                name: req.body.name,
+                simNumber: req.body.simNumber,
+                location: req.body.location
+            });
+            newDevice.save().then(device => {
+                res.send({device});
+            });
+        }
+    }).catch((err) => {
+        res.status(400).send();
+    })
+});
+
+router.put('/updateDevice', (req, res) => {
+    var device = new Device({
         name: req.body.name,
         simNumber: req.body.simNumber,
         location: req.body.location
-      });
-      newDevice.save().then(device => {
+    });
+    Device.findOneAndUpdate({'deviceId': req.body.deviceId}, {$set: device}, {new: true}).then((device) => {
         res.send({device});
-      });
-    }
-  }).catch((err) => {
-    res.status(400).send();
-  })
+    }).catch((err) => {
+        res.status(400).send();
+    });
 });
 
 router.get('/getDevices/:userId', (req, res) => {
-  Device.find({'userId': req.params.userId}).then(devices => {
-    res.send({devices});
-  }).catch((err) => {
-    res.status(400).send();
-  })
+    Device.find({'userId': req.params.userId}).then(devices => {
+        res.send({devices});
+    }).catch((err) => {
+        res.status(400).send();
+    })
 });
 
 module.exports = router;
